187a7653c17128662ff7189d1a95727eb98d1297
hocl1 <leon.hochwimmer@coop.ch>
2023-02-22 13:00:31 +0100

    minor changes

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import Home from "./components/Home/Home";
 import "./App.scss";
 
 function App() {
---
+++ portfolio/src/components/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "Home.scss";
+
+class Home extends React.Component {
+  render() {
+    return <h1>hello world</h1>;
+  }
+}
+
+export default Home;
