fb585c5d4eae253286fcdad41572503945db3d83
hocl1 <leon.hochwimmer@coop.ch>
2023-02-23 11:49:04 +0100

    init component Navigation

---
+++ portfolio/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from "react";
+import "./Navigation.scss";
+
+class Navigation extends React.Component {
+  render(): ReactNode {
+    return <nav>Navigation</nav>;
+  }
+}
+
+export default Navigation;
