4d31c49b3bfe009392e2e77328bb7ce1830c6cc0
hocl1 <leon.hochwimmer@coop.ch>
2023-02-23 11:49:23 +0100

    init component Head

---
+++ portfolio/src/components/Head/Head.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from "react";
+import { Helmet } from "react-helmet";
+import TabLight from "../../assets/tab_pref-light.png";
+import TabDark from "../../assets/tab_pref-dark.png";
+
+class Head extends React.Component {
+  render(): ReactNode {
+    return (
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>Beta</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href={TabLight}
+          media="(prefers-color-scheme: light)"
+        />
+        <link rel="icon" href={TabDark} media="(prefers-color-scheme: dark)" />
+      </Helmet>
+    );
+  }
+}
+
+export default Head;
