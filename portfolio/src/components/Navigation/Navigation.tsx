54080844706547bb5f3212301b344899ee99319c
hocl1 <leon.hochwimmer@coop.ch>
2023-02-24 16:26:09 +0100

    minor changes

--- portfolio/src/components/Navigation/Navigation.tsx
+++ portfolio/src/components/Navigation/Navigation.tsx
@@ -1,11 +1,29 @@
 import React, { ReactNode } from "react";
+import { Link } from "react-router-dom";
 import "./Navigation.scss";
 
 const actions = ["Home", "About", "Work", "Contact"];
 
 class Navigation extends React.Component {
   render(): ReactNode {
-    return <nav></nav>;
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/work">Work</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    );
   }
 }
 
