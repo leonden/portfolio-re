1456ed6d6ce02e56284d3c21df39be039e10e51f
hocl1 <leon.hochwimmer@coop.ch>
2023-02-24 16:25:59 +0100

    404 page

---
+++ portfolio/src/components/Error/ErrorPage.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+import "./ErrorPage.scss";
+
+class ErrorPage extends React.Component {
+  render(): ReactNode {
+    return (
+      <section id="error-page">
+        <h1>ErrorPage</h1>
+      </section>
+    );
+  }
+}
+
+export default ErrorPage;
