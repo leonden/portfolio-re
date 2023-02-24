f15c90435c59abf61d41e1a02f2c31d632c113a8
hocl1 <leon.hochwimmer@coop.ch>
2023-02-24 07:52:32 +0100

    imports

--- portfolio/src/components/Navigation/Navigation.tsx
+++ portfolio/src/components/Navigation/Navigation.tsx
@@ -1,5 +1,9 @@
 import React, { ReactNode } from "react";
 import "./Navigation.scss";
+import Home from "../Home/Home";
+import About from "../About/About";
+import Work from "../Work/Work";
+import Contact from "../Contact/Contact";
 
 class Navigation extends React.Component {
   render(): ReactNode {
