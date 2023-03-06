3f8c05288b3545579f159e90fd2fd7a2e8c7b006
hocl1 <leon.hochwimmer@coop.ch>
2023-03-06 14:05:08 +0100

    create Cursor component

---
+++ portfolio/src/components/Cursor/Cursor.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from "react";
+import "./Cursor.scss";
+
+class Cursor extends React.Component {
+  render(): ReactNode {
+    return <div className="cursor"></div>;
+  }
+}
+
+export default Cursor;
