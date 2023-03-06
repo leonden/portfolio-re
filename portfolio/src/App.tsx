895dbfbb3627667f431b69baa0a2a687c876a10c
hocl1 <leon.hochwimmer@coop.ch>
2023-03-06 11:51:03 +0100

    routing

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,35 +1,25 @@
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Work from "./components/Work/Work";
-import Contact from "./components/Contact/Contact";
-import ErrorPage from "./components/Error/ErrorPage";
+import Home from "./components/pages/Home/Home";
+import About from "./components/pages/About/About";
+import Work from "./components/pages/Work/Work";
+import Contact from "./components/pages/Contact/Contact";
+import ErrorPage from "./components/pages/Error/ErrorPage";
 import Navigation from "./components/Navigation/Navigation";
 import Head from "./components/Head/Head";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 
 function App() {
   return (
     <>
       <Head></Head>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" index element={<Home />}></Route>
-          <Route path="/about" element={<About />}>
-            <About></About>
-          </Route>
-          <Route path="/work" element={<Work />}>
-            <Work></Work>
-          </Route>
-          <Route path="/contact" element={<Contact />}>
-            <Contact></Contact>
-          </Route>
-          <Route path="/*" element={<ErrorPage />}>
-            <ErrorPage></ErrorPage>
-          </Route>
-        </Routes>
-      </BrowserRouter>
       <Navigation></Navigation>
+      <Routes>
+        <Route path="/" index element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/work" element={<Work />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/*" element={<ErrorPage />}></Route>
+      </Routes>
     </>
   );
 }
